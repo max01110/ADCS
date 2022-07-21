715b149ac42666feb81f7c26d9cad3bfd0b6c58f
Max M <46329167+max01110@users.noreply.github.com>
2022-07-20 23:49:57 -0400

    Create README.m

---
+++ GNC/README.m
@@ -0,0 +1,24 @@
+# ADCS-GNC
+
+**GNC For UTAT FINCH**
+
+**State Estimation**
+
+- [ ] Extended Kalman Filter
+  - [ ] Sun Sensor
+  - [ ] IMU (Gyroscope & Magnetometer)
+  - [ ] Star Tracker
+ 
+**Attitude Control**
+
+- [ ] Controller
+  - [ ] Astrodynamical model
+  - [ ] Control Law
+- [ ] Modes
+  - [ ] Detumbling
+  - [ ] IDLE Mode
+  - [ ] Course Pointing
+  - [ ] Normal Mode
+  - [ ] Safe Mode
+  - [ ] Target Tracking
+
